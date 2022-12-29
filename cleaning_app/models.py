6b3784ba615f3cf00d1aee0d8caf2cbb3cc248bf
Dmitry Kalinin <mitya.kalinin1@gmail.com>
2022-12-29 16:26:50 +0700

    changed period field

--- cleaning_app/models.py
+++ cleaning_app/models.py
@@ -31,32 +31,19 @@ class Slave(models.Model):
 class Chore(models.Model):
     name = models.CharField(max_length=250)
     date = models.DateTimeField('date done', null=True)
+    period = models.PositiveIntegerField('period count', null=True)
 
     class PeriodChoices(models.TextChoices):
-        DAY = 'D', _('Once a day')
-        WEEK = 'W', _('Once a week')
-        MONTH = 'M', _('Once a month')
-        YEAR = 'Y', _('Once a year')
-        ONCE = 'O', _('Once')
-        CUSTOM = 'C', _('Custom')
+        DAY = 'D', _('days')
+        WEEK = 'W', _('weeks')
+        MONTH = 'M', _('months')
+        YEAR = 'Y', _('years')
+        ONCE = 'O', _('once')
 
-    period = models.CharField(
+    period_type = models.CharField(
         max_length=1,
         choices=PeriodChoices.choices,
         default=PeriodChoices.WEEK,
-    )
-
-    class ConditionChoices(models.TextChoices):
-        AWFUL = 'A', _('Awful')
-        BAD = 'B', _('Bad')
-        NORMAL = 'N', _('Normal')
-        GOOD = 'G', _('Good')
-        EXCELLENT = 'E', _("Excellent")
-
-    condition = models.CharField(
-        max_length=1,
-        choices=ConditionChoices.choices,
-        default=ConditionChoices.NORMAL,
     )
 
     class EffortChoices(models.TextChoices):
