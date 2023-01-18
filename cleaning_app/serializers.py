5d910353ea93c46dfe2ad12f6c807c010205166f
Dmitry Kalinin <mitya.kalinin1@gmail.com>
2023-01-19 06:16:43 +0700

    that's more stupid than before

--- cleaning_app/serializers.py
+++ cleaning_app/serializers.py
@@ -102,17 +102,3 @@ class FamilySerializer(serializers.ModelSerializer):
     class Meta:
         model = Family
         fields = '__all__'
-
-#
-#
-# # Register Serializer
-# class RegisterSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('id', 'username', 'email', 'password')
-#         extra_kwargs = {'password': {'write_only': True}}
-#
-#     def create(self, validated_data):
-#         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
-#
-#         return user
