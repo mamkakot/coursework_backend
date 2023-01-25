fa7336d5e85b7724c3a06296a67a76f156ac0ee8
dmitry <mitya.kalinin1@gmail.com>
2023-01-26 01:29:55 +0700

    changes some views

--- cleaning_app/serializers.py
+++ cleaning_app/serializers.py
@@ -14,11 +14,14 @@ class ChoreSerializer(serializers.ModelSerializer):
 
 class UserRegistrationSerializer(BaseUserRegistrationSerializer):
     class Meta(BaseUserRegistrationSerializer.Meta):
-        fields = ('id', 'email', 'username', 'password',)
+        fields = ['id', 'email', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create(**validated_data)
-        print(user.username)
+        user = User(email=validated_data['email'],
+                    username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
         Slave.objects.create(user=user)
         return user
 
